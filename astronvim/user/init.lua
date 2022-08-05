358109f165e4cbfceedc7c6b324d37f8cafea7a4
JC <Keitokuch@users.noreply.github.com>
2022-08-05 16:29:14 -0700

    nvim add pylsp config

--- astronvim/user/init.lua
+++ astronvim/user/init.lua
@@ -278,6 +278,21 @@ local config = {
             vim.cmd [[PyrightOrganizeImports]]
           end, { desc = "Organize imports", buffer = bufnr })
         end
+      },
+      pylsp = {
+        settings = {
+          pylsp = {
+            plugins = {
+              flake8 = {enabled=true, maxLineLength=100, ignore={'E203'}},
+              pylint = {args = {'--ignore=E231', '-'}, enabled=true, debounce=200},
+              pycodestyle={
+                enabled=false,
+                -- ignore={'E501', 'E231'},
+                maxLineLength=100},
+              },
+              yapf={enabled=true}
+            }
+          }
       }
       -- example for addings schemas to yamlls
       -- yamlls = {
