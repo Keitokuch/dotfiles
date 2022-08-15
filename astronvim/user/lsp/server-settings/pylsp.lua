01d9697d152255e318c8fac0b77c488b568188e9
JC <Keitokuch@users.noreply.github.com>
2022-08-15 12:23:56 -0700

    misc update

--- astronvim/user/lsp/server-settings/pylsp.lua
+++ astronvim/user/lsp/server-settings/pylsp.lua
@@ -2,7 +2,7 @@ return {
   settings = {
     pylsp = {
       plugins = {
-        flake8 = {enabled=true, maxLineLength=100, ignore={'E203'}},
+        flake8 = {enabled=true, maxLineLength=100, ignore={'E203', 'F401', 'F541', 'F841'}},
         pylint = {args = {'--ignore=E231', '-'}, enabled=true, debounce=200},
         pycodestyle={
           enabled=false,
