f8b1ca5bb16647a4a1c48f49fc21c40b935b4404
JC <Keitokuch@users.noreply.github.com>
2023-06-21 10:39:11 -0700

    Update nvim dir start

--- astronvim/user/init.lua
+++ astronvim/user/init.lua
@@ -87,40 +87,49 @@ return {
     -- }
     vim.api.nvim_create_augroup("user_start", { clear = true })
     vim.api.nvim_create_augroup("user_exit", { clear = true })
-    local has_neotree = is_available("neo-tree")
+    local has_neotree = is_available("neo-tree.nvim")
     local has_nvimtree = is_available("nvim-tree.lua")
     -- Handle session save/load during startup
     if is_available "neovim-session-manager" then
       if has_neotree then
         vim.api.nvim_del_augroup_by_name("neotree_start")
       end
-      local tree_show, tree_focus = "", ""
-      tree_show = has_neotree and "Neotree show" or tree_show
-      tree_focus = has_neotree and "Neotree focus" or tree_focus
-      tree_show = has_nvimtree and "NvimTreeOpen" or tree_show
-      tree_focus = has_neotree and "NvimTreeFocus" or tree_focus
+      local tree_show, tree_focus = nil, nil
+      if has_neotree then
+        tree_show = function()
+          vim.cmd("Neotree show")
+        end
+        tree_focus = function()
+          vim.cmd("Neotree focus")
+        end
+      elseif has_nvimtree then
+        tree_show = function()
+          require('nvim-tree.api').tree.toggle(false, true)
+        end
+        tree_focus = function()
+          require('nvim-tree.api').tree.toggle(false, true)
+        end
+      end
       local session_get_f = function(dir)
         return require 'session_manager.config'.dir_to_session_filename(dir)
       end
 
       local dir_start = function()
-        if vim.fn.argc() == 1 and not vim.g.started_with_stdin and vim.fn.isdirectory(vim.v.argv[2]) ~= 0 then
+        if vim.fn.argc() == 1 and not vim.g.started_with_stdin and vim.fn.isdirectory(vim.fn.argv()[1]) ~= 0 then
           -- Started with dir name
-          vim.loop.chdir(vim.v.argv[2])
+          vim.loop.chdir(vim.fn.argv()[1])
           vim.cmd [[%argd]]
           local cwd = vim.loop.cwd()
           local session_f = session_get_f(cwd)
-          print(cwd)
           if session_f:exists() then
             require 'session_manager'.load_current_dir_session()
-            vim.defer_fn(function()
-              vim.cmd("ShowTree")
-            end, 30)
+            print("Load session in", cwd)
+            vim.defer_fn(tree_show, 30)
           else
-            -- require("neo-tree.setup.netrw").hijack()
+            print("New session", cwd)
             require 'session_manager.utils'.is_session = true
             vim.cmd [[bd]]
-            vim.cmd("FocusTree")
+            tree_focus()
           end
         elseif vim.fn.argc() == 0 then
           -- Started without argument
@@ -128,17 +137,18 @@ return {
           local session_f = session_get_f(cwd)
           if session_f:exists() then
             require 'session_manager'.load_current_dir_session()
+            print("Load session", cwd)
             vim.defer_fn(function()
               -- Open tree when only a empty buffer is opened in session
               if #vim.api.nvim_list_bufs() == 1 and vim.api.nvim_buf_line_count(0) == 1 then
-                vim.cmd("FocusTree")
+                tree_show()
               end
             end, 10)
           else
             -- Create new session for cwd during 0-arg start
-            print("New:", cwd)
+            print("New session", cwd)
             require 'session_manager.utils'.is_session = true
-            vim.cmd("FocusTree")
+            tree_focus()
           end
         end
       end
@@ -147,13 +157,10 @@ return {
         group = "user_start",
         callback = dir_start
       })
+      vim.api.nvim_create_autocmd({ 'VimLeavePre' }, {
+        group = "user_exit",
+        callback = require 'session_manager'.autosave_session
+      })
     end
-
-    local map = vim.keymap.set
-    map("n", "<leader>l", function()
-        require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1))
-      end,
-      { nowait = true, desc = "Previous buffer" }
-    )
   end
 }
