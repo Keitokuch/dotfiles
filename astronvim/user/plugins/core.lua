f890933e0d81f1695c48c957a856ba4fe149a691
JC <Keitokuch@users.noreply.github.com>
2023-08-18 16:15:02 -0700

    asnvim line config

--- astronvim/user/plugins/core.lua
+++ astronvim/user/plugins/core.lua
@@ -26,7 +26,7 @@ return {
   --
   {
     "Shatur/neovim-session-manager",
-    event = "BufWritePost",
+    event = "VimEnter",
     cmd = "SessionManager",
     config = require "user.plugins.configs.session"
   },
@@ -73,7 +73,7 @@ return {
   {
     "folke/which-key.nvim",
     config = function(plugin, opts)
-      require "plugins.configs.which-key"(plugin, opts) -- include the default astronvim config that calls the setup call
+      require "plugins.configs.which-key" (plugin, opts) -- include the default astronvim config that calls the setup call
       -- Add bindings which show up as group name
       local wk = require "which-key"
       wk.register({
