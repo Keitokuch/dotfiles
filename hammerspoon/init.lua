d6869cc2fda08b3d7662760ac414e6ad49b30067
JC <Keitokuch@users.noreply.github.com>
2024-12-26 16:15:32 -0800

    Upgrade Hammerspoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,29 +1,22 @@
-hs.application.enableSpotlightForNameSearches(true)
+-- hs.application.enableSpotlightForNameSearches(true)
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
-  hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
-end)
-
-
+-- AppToggle
 local app_mod = "alt"
 local apps = {
   { app_mod, "z", "Notion" },
-  -- { app_mod, "z", "Mark Text" },
-  { app_mod, "x", "PDF Expert" },
-  { app_mod, "c", "Google Chrome" },
-  { app_mod, "v", "WeChat" },
+  { app_mod, "c", "company.thebrowser.Browser" },
+  { app_mod, "v", "Slack" },
   { app_mod, "b", "Finder" },
   { app_mod, "n", "Numi" },
   { app_mod, "d", "Dash" },
-  { app_mod, "f", "Opera" },
-  { app_mod, "s", "Things" },
-  { app_mod, "space", "iTerm2" },
-  { { "alt", "shift" }, "c", "Calendar" }
+  { app_mod, "s", "Reminders" },
+  { app_mod, "space", "com.googlecode.iterm2" },
+  { { "alt" }, "e", "Calendar" }
 }
-
 hs.loadSpoon("AppToggle")
 spoon.AppToggle:setMap(apps)
 
+-- ShiftIt
 local shiftit = {
   left = {{ 'ctrl', 'cmd' }, 'left' },
   right = {{ 'ctrl', 'cmd' }, 'right' },
@@ -35,15 +28,17 @@ local shiftit = {
 hs.loadSpoon("ShiftIt")
 spoon.ShiftIt:bindHotkeys(shiftit)
 
-hs.loadSpoon("AppKeyMap")
-spoon.AppKeyMap:setAppMap("Google Chrome", {
-    { {'ctrl'}, 'c', {}, 'escape' },
-  })
-spoon.AppKeyMap:setAppMap("Dash", {
-    { {'ctrl'}, 'j', {}, 'down' },
-    { {'ctrl'}, 'k', {}, 'up' }
-  })
-spoon.AppKeyMap:setAppMap("PDF Expert", {
-    { {'ctrl'}, 'j', {'ctrl'}, 'down' },
-    { {'ctrl'}, 'k', {'ctrl'}, 'up' }
-  })
+-- hs.loadSpoon("AppKeyMap")
+-- spoon.AppKeyMap:setAppMap("Google Chrome", {
+--     { {'ctrl'}, 'c', {}, 'escape' },
+--   })
+-- spoon.AppKeyMap:setAppMap("Dash", {
+--     { {'ctrl'}, 'd', {}, 'down' },
+--     { {'ctrl'}, 'u', {}, 'up' }
+--   })
+-- spoon.AppKeyMap:setAppMap("PDF Expert", {
+--     { {'ctrl'}, 'd', {'ctrl'}, 'down' },
+--     { {'ctrl'}, 'u', {'ctrl'}, 'up' }
+--   })
+
+status, err = pcall(require, "initLocal")
