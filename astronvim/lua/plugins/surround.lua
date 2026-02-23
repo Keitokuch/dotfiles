a133cdb2d5087b16bbf069d8a6455ffcc564c9d4
JC <keitokuch@gmail.com>
2026-02-23 10:55:49 -0800

    [nvim] Update plugins

--- astronvim/lua/plugins/surround.lua
+++ astronvim/lua/plugins/surround.lua
@@ -1,14 +1,11 @@
 return {
 	"kylechui/nvim-surround",
-	version = "*", -- Use for stability; omit to use `main` branch for the latest features
+	version = "^4.0.0", -- Use for stability; omit to use `main` branch for the latest features
 	event = "VeryLazy",
+	-- Optional: See `:h nvim-surround.configuration` and `:h nvim-surround.setup` for details
 	config = function()
 		require("nvim-surround").setup({
-			-- Configuration here, or leave empty to use defaults
-			keymaps = {
-				visual = "s",
-				visual_line = "S",
-			},
+			-- Put your configuration here
 		})
 	end,
 }
